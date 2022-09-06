40161084d5ce33b92ffac7547ec0bff3a0e9380c
Vadim A <aypych@gmail.com>
2022-09-07 01:06:51 +0500

    Added all tests

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mockStatic;
 
 public class HorseTest {
 
@@ -22,13 +27,13 @@ public class HorseTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings =  {"", " ", "  ", "   ", "\t", "\n"})
-    public void blankNameExceptions(String name){
+    @ValueSource(strings = {"", " ", "  ", "   ", "\t", "\n"})
+    public void blankNameExceptions(String name) {
         assertThrows(IllegalArgumentException.class, () -> new Horse(name, 1., 1.));
     }
 
     @ParameterizedTest
-    @ValueSource (strings = {"", " ", "  ", "   ", "\t", "\n"})
+    @ValueSource(strings = {"", " ", "  ", "   ", "\t", "\n"})
     public void blankNameWithMessage(String name) {
         try {
             new Horse(name, 1., 1.);
@@ -62,6 +67,42 @@ public class HorseTest {
             new Horse("name", 1., -1.);
         } catch (IllegalArgumentException e) {
             assertEquals("Distance cannot be negative.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void getNameTest() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse("horse", 1., 1.);
+        Field name = Horse.class.getDeclaredField("name");
+        name.setAccessible(true);
+        String nameValue = (String) name.get(horse);
+        assertEquals("horse", nameValue);
+    }
+
+    @Test
+    public void getSpeedTest() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse("horse", 1., 1.);
+        assertEquals("horse", horse.getName());
+    }
+
+    @Test
+    public void getDistanceTest() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse("horse", 1., 1.);
+        assertEquals(1, horse.getSpeed());
+    }
+
+    @Test
+    public void getDistanceZeroTest() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse("horse", 1.);
+        assertEquals(0, horse.getDistance());
+    }
+
+    @Test
+    public void moveUsesGetRandom() {
+        try (MockedStatic<Horse> mockedStatic = mockStatic(Horse.class)) {
+            new Horse("horse", 1, 1);
+            mockedStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+
         }
     }
 }
